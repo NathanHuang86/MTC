21d4a68c6e2277124dfadda5bdf7f792ecdc8250
WolfgangS <wcstrubbe@gmail.com>
2022-08-17 07:11:02 -0500

    ominous mapping finished

--- src/Singleton/Gobal.gd
+++ src/Singleton/Gobal.gd
@@ -1,10 +1,6 @@
 extends Node
 
 var gamedata = {
-<<<<<<< Updated upstream
 	"intScene" : 0,
-=======
-	"intScene" : 5,
->>>>>>> Stashed changes
 	"strProtagName" : "Player",
 	"scriptLine" : null}
